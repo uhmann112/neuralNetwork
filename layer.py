ecd80f5a983ba505435de203c4fd1cf3909c7ef1
maso <marcel.uhlemann112@gmail.com>
2026-03-07 18:49:18 +0100

    more fixes startet implementing backpropagation on single neuron level)

--- layer.py
+++ layer.py
@@ -7,6 +7,8 @@ class Layer:
 		self.layerSize=layerSize
 		self.numInputs=numInputs
 		self.neurons = [Neuron(self.numInputs) for i in range(self.layerSize)]
+		self.deltas = []
+
 
 	def process(self,inputs):
 		output=[]
@@ -14,3 +16,16 @@ class Layer:
 		for n in self.neurons:
 			output.append(n.forward(inputs))
 		return output
+
+	def backwards(self):
+		for n in self.neurons:
+			n.backwards(1.0)
+			self.deltas.append(n.delta.item())
+
+
+inputs = np.array([-299,3,455,-123])
+l= Layer(4,4)
+l.process(inputs)
+l.backwards()
+print(l.deltas)
+
--- neuron.py
+++ neuron.py
@@ -4,13 +4,30 @@ import numpy as np
 def sigmoid(x):
 	return 1 / (1 + np.exp(-x))
 
+def sigmoidDerivative(prevOut):
+	s = sigmoid(prevOut)
+	return s * (1 - s)
+
+def delta(exepected):
+	return (prevOut-expected)*sigmoidDerivative
+
+
+
 
 class Neuron:
-	def __init__(self,numInputs):
+	def __init__(self,numInputs,lr=0.1):
 		self.weights=np.random.rand(1,numInputs)
 		self.bias = np.random.randint(5)
+		self.lr=lr
 
 	def forward(self,inputs):
-		total= np.dot(self.weights,inputs)+self.bias
-		return sigmoid(total).item()
+		self.inputs = inputs
+		self.weightedSum= np.dot(self.weights,self.inputs)+self.bias
+		self.output = sigmoid(self.weightedSum).item()
+		return self.output
+
+	def backwards(self, expected):
+		self.delta = (self.output - expected) * sigmoidDerivative(self.weightedSum)
+		self.weights -= self.lr * self.delta * self.inputs
+		self.bias -= self.lr * self.delta
 
