0d38b7e67be674e3350595b1df2fd2e51c22cc39
maso <marcel.uhlemann112@gmail.com>
2026-03-08 18:15:52 +0100

    nice!

--- main.py
+++ main.py
@@ -6,10 +6,18 @@ from layer import Layer
 
 iterations=1000
 
-expected = [0,1]
+def xor_level(bits):
+    # XOR ist 1, wenn die Anzahl der Einsen ungerade ist
+    return [1.0,0.0]  if sum(bits) % 2 == 1 else [0.0,1.0]
+
+
+
+
+
 inputs = np.array([1,1,1,0])
 numNeurons= len(inputs)
 numInputs=len(inputs)
+expected = xor_level(inputs)
 
 
 #lazer werden initialisiert 
@@ -19,7 +27,7 @@ l3 = Layer(numNeurons,numInputs)
 l4 = Layer(2,numInputs)
 
 
-
+print(expected)
 for i in range(iterations):
 
 	out1=l1.process(inputs)
@@ -34,3 +42,5 @@ for i in range(iterations):
 
 	if i%100==0:
 		print(out4)
+
+print(expected)
