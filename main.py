4071ac7de0c3f13138a2331e8e53daf40df08f88
uhmann112 <marce.uhlemann112@gmail.com>
2026-03-10 09:54:19 +0100

    almost finished visualization

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import numpy as np
 
-import plot
+#import plot
 from neuron import Neuron
 from layer import Layer
 
 
-iterations=1000
+iterations=5
 
 def xor_level(bits):
     # XOR ist High, wenn die Anzahl der Einsen ungerade ist
@@ -15,9 +15,9 @@ def xor_level(bits):
 
 
 
-inputs,expected = plot.generate_data(n=200)
-numNeurons= 20
-numInputs=len(inputs[0])
+
+
+
 
 
 
@@ -26,7 +26,7 @@ lazer werden initialisiert
 Layer nimmt 
 	1. zahl der neuronen
 	2. form der inputs
-	3. nutzt softMax ? sonst sigmoid activation
+	3. nutzt softMax ? sonst sigmoid activationfunktion
 '''
 input_size = 2
 hidden1 = 8
@@ -39,25 +39,33 @@ l3 = Layer(hidden1, hidden2, False)   # Softmax
 l4 = Layer(output_size, hidden1, True)
 
 
+def predict(point):
+    o1 = l1.process(point)
+    o2 = l2.process(o1)
+    o3 = l3.process(o2)
+    o4 = l4.process(o3)
+    return o4
+
+def runNetwork(data):	
+	for i in range(iterations):
+		data=np.array(data,dtype=float)
+		for x in data:
+			CurrentInput = x[:2]
+			expected = [1.0, 0.0] if x[2] == 1 else [0.0, 1.0]
+
+			out1=l1.process(CurrentInput)
+			out2=l2.process(out1)
+			out3=l3.process(out2)
+			out4=l4.process(out3)
+
+			l4.backwardsOut(expected)
+			l3.backwards(l4.deltas,l4.weights)
+			l2.backwards(l3.deltas,l3.weights)
+			l1.backwards(l2.deltas,l2.weights)
+		if iterations %10 ==0:
+			print(out4)
+			
+
+	print(expected)
 
 
-
-for i in range(iterations):
-	CurrentInput = inputs[i-1]
-	currenExpected= [1.0,0.0] if expected[i]== 1  else [0.0,1.0]
-
-	out1=l1.process(CurrentInput)
-	out2=l2.process(out1)
-	out3=l3.process(out2)
-	out4=l4.process(out3)
-
-	l4.backwardsOut(currenExpected)
-	l3.backwards(l4.deltas,l4.weights)
-	l2.backwards(l3.deltas,l3.weights)
-	l1.backwards(l2.deltas,l2.weights)
-	
-	if i % 100 == 0:
-	    print([float(x) for x in out4])
-
-
-print(expected)
--- plot.py
+++ plot.py
@@ -2,50 +2,87 @@ import matplotlib
 matplotlib.use("Qt5Agg")
 import matplotlib.pyplot as plt
 import numpy as np
+import main
+import cProfile
 
+
+x =[]
+y=[]
+plt.ion()
 
 def f(x):
     return -(0.0001 * x**3) + 50
 
 
-def generate_data(n=200):
+def generate_data(n):
     x = np.random.uniform(0, 100, n)
     y = np.random.uniform(0, 100, n)
-    # Labels: 1 = über der Funktion, 0 = unter der Funktion
-    labels = (y > f(x)).astype(int)
-    # Trainingsmatrix
-    X = np.column_stack((x, y))
-    return X, labels
+    lab = np.array([1 if f(xi) <= yi else 0 for xi, yi in zip(x, y)])
+    output=[]
+    for i in range(n):
+        storage=[]
+        output.append([x[i],y[i],lab[i]])
+
+    return output , x, y, lab
+
+def plotTestPoints(x,y):
+    plt.scatter(x, y, c=lab, cmap="coolwarm")
+    plt.plot(np.linspace(0,100,200), f(np.linspace(0,100,200)), color="black")
+    plt.title("Generated Data + f(x)")
+
+def make_grid(xmin=0, xmax=100, ymin=0, ymax=100, step=1):
+    xs = np.arange(xmin, xmax, step)
+    ys = np.arange(ymin, ymax, step)
+
+    X, Y = np.meshgrid(xs, ys)
+
+    # zu einer Liste von Punkten machen
+    grid = np.column_stack((X.ravel(), Y.ravel()))
+    return grid
+
+grid = make_grid(step=1)
 
 
-# ---------------------------------------------------------
-# 3) Plotten der Funktion + Punkte + Bereiche
-# ---------------------------------------------------------
-def plot_data(X, labels):
-    fig, ax = plt.subplots()
-
-    # Funktion plotten
-    xs = np.linspace(0, 100, 500)
-    ax.plot(xs, f(xs), color="black", linewidth=2)
-
-    # Punkte einfärben
-    colors = np.where(labels == 1, "red", "blue")
-    ax.scatter(X[:, 0], X[:, 1], c=colors)
-
-    # Bereiche einfärben
-    ax.fill_between(xs, f(xs), 0, color="blue", alpha=0.15)
-    ax.fill_between(xs, f(xs), 100, color="red", alpha=0.15)
-
-    ax.set_xlim(0, 100)
-    ax.set_ylim(0, 100)
-    ax.set_title("Trainingsdaten + Funktion")
-
-    plt.show()
 
 
-# ---------------------------------------------------------
-# 4) Ausführen
-# ---------------------------------------------------------
-X, labels = generate_data(n=200)
-plot_data(X, labels)
+
+data, x, y, lab = generate_data(20)
+plotTestPoints(x,y)
+epochs=20
+
+for i in range(epochs):
+    
+    prediction = np.array([main.predict(x) for x in grid])
+
+    # Wahrscheinlichkeit für Klasse 1
+    p = prediction[:, 1]
+
+    # Punkte nahe 0.5 behalten
+    tolerance = 0.05
+    mask = np.abs(p - 0.5) < tolerance
+
+    # WICHTIG: die GRID-PUNKTE filtern, nicht die Predictions
+    boundary_points = grid[grid]
+
+    # Nach x sortieren
+    boundary_points = boundary_points[boundary_points[:, 0].argsort()]
+
+    # Linie plotten
+    plt.plot(boundary_points[:, 0], boundary_points[:, 1], color="red")
+
+    plt.pause(0.001)
+    main.runNetwork(data)
+
+#cProfile.run("main.runNetwork(data)")
+
+
+#[Finished in 90.7s]
+
+
+
+
+
+
+
+
 
